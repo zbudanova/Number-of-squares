5664fd427e9f2a92ea0128a09e4d9162fc485408
Zarina Budanova <zarina.nurbaeva@gmail.com>
2021-10-21 18:20:38 +0500

    Changed 99 on 100 in SQEService.java

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -3,7 +3,7 @@ package ru.netology.sqr;
 public class SQRService {
     public int numberOfSquares (int lowerBound, int upperBound) {
         int counter = 0;
-        for (int i = 10; i < 99; i++) {
+        for (int i = 10; i < 100; i++) {
             if (i * i >= lowerBound) {
                 if (i * i <= upperBound) {
                     counter = counter +1;
